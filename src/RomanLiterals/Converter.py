93b908ed2882299a295706888dc2472ef025ebee
Lucas Weber <lucas.weber@fau.de>
2022-04-25 11:04:41 +0200

    [UP] add literal to number conversion

--- src/RomanLiterals/Converter.py
+++ src/RomanLiterals/Converter.py
@@ -1,5 +1,5 @@
 from RomanLiterals.SortingDict import SortingDict
-
+from math import log10
 
 # literature collection
 """
@@ -81,7 +81,7 @@ def number2literal(input_number: int, complete_literals: dict):
     return complete_literal
 
 
-def to_literal(number):
+def to_literal(number: int):
     # a dict of valid literals
     valid_literals = SortingDict({1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'})
 
@@ -91,6 +91,73 @@ def to_literal(number):
     return number2literal(number, complete_literals_dict)
 
 
+def from_literal(literal: str):
+
+    # a dict of valid literals and their numbers
+    valid_literals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # reverse the dict to get valid numbers
+    valid_numbers = {value: key for key, value in valid_literals.items()}
+
+    # make all characters from the string upper case
+    literal = literal.upper()
+
+    # go through the string
+    skip_literal = False
+    result = 0
+    last_value = 0
+    for counter, char in enumerate(literal[::-1], start=1):
+
+        # check whether we need to skip the current literals (because we already processed it with a subtraction
+        if skip_literal:
+            skip_literal = False  # next literal is interesting again
+            continue
+        else:
+
+            # get the current value of the position
+            current_value = valid_literals[char]
+
+            # check whether its is smaller than the last added value (not allowed)
+            # e.g. IIX is not allowed if we are at position -3 (first I). In this case I < 9 (IX) we processed right
+            # before -> raise ValueError
+            if current_value < last_value:
+                raise ValueError(f'[{literal=}] is not a valid input as the symbols are not in order!')
+
+            # go one step deeper and check whether we need to do subtraction
+            # also we need to check to not run out of range when current value is the first character in the string
+            if counter < len(literal):
+                next_value = valid_literals[literal[-counter-1]]
+
+                # check whether next value is smaller
+                if next_value < current_value:
+
+                    # check whether this subtraction is valid (only allowed to change each tens digit)
+                    # meaning that IC is not allowed for example. Only one power of ten is allowed between them
+                    if log10(current_value) - log10(next_value) > 1:
+                        raise ValueError(f'[{literal=}]'
+                                         f' is not a valid input as you can not subtract over tens digit border!')
+
+                    # check whether this subtracted number makes sense
+                    # for example DM for 500 makes no sense at it is shorter with just D
+                    alternative_literal = valid_numbers.get(current_value - next_value)
+                    if alternative_literal is not None:
+                        raise ValueError(f'[{literal=}] is not a valid input as this number can be simplified to'
+                                         f' {literal[:-counter-1] + alternative_literal}!')
+
+                    # make the subtraction and raise the skip next flag
+                    current_value = current_value - next_value
+                    skip_literal = True
+
+            # add the number to the result
+            result += current_value
+
+            # save the current added value to check for valid literals
+            # e.g. IIX is not allowed
+            last_value = current_value
+
+    return result
+
+
 if __name__ == '__main__':
     # this code won't be tested since its is just demonstration (not necessary)
     print(to_literal(199))
+    print(from_literal('MCMXCIX'))
