cea4dd19446dff67de9e92b84df1749322641070
Lucas Weber <lucas.weber@fau.de>
2022-04-25 11:12:20 +0200

    [ADD] add access counter dict code

--- src/RomanLiterals/SortingDict.py
+++ src/RomanLiterals/SortingDict.py
@@ -47,3 +47,68 @@ class SortingDict(dict):
     def copy(self):
         return SortingDict(self)
 
+
+class AccessCounterDict(dict):
+
+    def __init__(self, initial_dictionary: dict):
+        super().__init__(initial_dictionary)
+
+        # create an additional counter dict
+        self.counter = dict.fromkeys(initial_dictionary.keys(), 0)
+        self.max_key = ''
+
+    def __getitem__(self, key):
+
+        # get the value
+        val = super().__getitem__(key)
+
+        # increase the counter
+        self._counter_increase(key)
+
+        return val
+
+    def _counter_increase(self, key):
+        """
+        We need this function in order to also keep track of the maximum counter while increasing.
+        :param key: The keyword for the dictionary element we are accessing
+        :return: None
+        """
+
+        # increase the counter
+        self.counter[key] += 1
+
+        # keep track of maximum counter
+        if self.counter[key] > self.counter.get(self.max_key, -1):
+            self.max_key = key
+
+    def __setitem__(self, key, val):
+
+        # set the new value
+        super().__setitem__(key, val)
+
+    def __repr__(self):
+
+        # make the representation of this dict
+        repr_str = '{' + \
+                   ''.join([f'{repr(key)} [{repr(self.counter[key])}]: {repr(value)}, '
+                            for key, value in super().items()]) + \
+                   '}'
+        return repr_str
+
+    def get(self, key, value=None):
+        res = super().get(key, value)
+        if key in super().keys():
+            self._counter_increase(key)
+        return res
+
+    def update(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).items():
+            super().__setitem__(k, v)
+            if k not in super().keys():
+                self.counter[k] = 0
+
+    def get_access_counter(self, key):
+        return self.counter[key]
+
+    def max(self):
+        return self.counter[self.max_key]
